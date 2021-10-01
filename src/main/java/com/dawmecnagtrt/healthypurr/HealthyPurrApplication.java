3e052bc69a7400478f9232959327621e2ef93cc5
Flavio Saavedra <f.saavedra1810@gmail.com>
2021-10-01 16:49:28 -0500

    first commit

---
+++ src/main/java/com/dawmecnagtrt/healthypurr/HealthyPurrApplication.java
@@ -0,0 +1,13 @@
+package com.dawmecnagtrt.healthypurr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyPurrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HealthyPurrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dawmecnagtrt/healthypurr/HealthyPurrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dawmecnagtrt.healthypurr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthyPurrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
