adfa81a46243c423ded48ea80b1db0bcf3f96d7f
Flavio Saavedra <f.saavedra1810@gmail.com>
2021-11-19 16:15:45 -0500

    Update SecurityConfig.java

--- src/main/java/com/dawmecnagtrt/healthypurr/config/SecurityConfig.java
+++ src/main/java/com/dawmecnagtrt/healthypurr/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/csrf").permitAll()
                 .antMatchers("/auth").permitAll()
                 .antMatchers(HttpMethod.POST,"/users").permitAll()
+                .antMatchers("/roles/**").permitAll()
 //                .antMatchers("/roles/**").hasAnyAuthority("ADMIN")
                 .anyRequest().authenticated();
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
