9991b890abe101d248cc4cfc76f4acb1d0481212
Sebastian Pinillos <48972318+SebastianPZ@users.noreply.github.com>
2022-06-19 10:24:12 -0500

    Update HealthyPurrApplication.java

--- src/main/java/com/dawmecnagtrt/healthypurr/HealthyPurrApplication.java
+++ src/main/java/com/dawmecnagtrt/healthypurr/HealthyPurrApplication.java
@@ -7,6 +7,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Healthy-Purr", version = "1.0", description = "API de Healthy-Purr"))
